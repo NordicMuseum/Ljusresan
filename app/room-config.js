cb4ea59f37486d8455d36839d2b20e054aba1df2
Pär Thernström <par@earthpeople.se>
2016-09-15 11:12:05 +0200

    update room config to be more realistic

--- app/room-config.js
+++ app/room-config.js
@@ -1,17 +1,55 @@
 module.exports = [
+
     {
+        name: "rum 1",
+        description: "Startrummet, touch = börjar ny session",
+        taskOrder: "any", // allAnyOrder, allInOrder, any
+        taskIDs: [
+            "room-1-task-1"
+        ]
+    },
 
-        "name": "rum 1",
-        "stations": [
+    {
+        name: "rum 2",
+        description: "",
+        taskOrder: "allAnyOrder",
+        taskIDs: [
+            "room-2-task-1",
+            "room-2-task-2"
+        ]
+    },
 
-        ],
-        // allAnyOrder, allOrder
-        "type": "allAnyOrder "
+    {
+        name: "rum 3",
+        description: "",
+        taskOrder: "allAnyOrder",
+        taskIDs: [
+            "room-3-task-1",
+            "room-3-task-2",
+            "room-3-task-3"
+        ]
 
     },
 
     {
-        "name": "rum 2",
+        name: "rum 4",
+        description: "",
+        taskOrder: "allInOrder",
+        taskIDs: [
+            "room-3-task-1",
+            "room-3-task-2",
+            "room-3-task-3",
+            "room-3-task-4"
+        ]
+    },
 
+    {
+        name: "rum 5",
+        description: "ipad-rummet",
+        taskOrder: "any",
+        taskIDs: [
+            "room-4-task-1"
+        ]
     }
+
 ];
