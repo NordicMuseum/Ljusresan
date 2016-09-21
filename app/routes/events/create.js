350e3548c6b1cfffbb83d25beed349e374b74329
Adam A <adam@fapfap.se>
2016-09-21 17:14:19 +0200

    Create event inside try block

--- app/routes/events/create.js
+++ app/routes/events/create.js
@@ -2,10 +2,10 @@ const config = require('../../../config')
 const r = require('rethinkdbdash')(config.rethinkdb)
 
 module.exports = function * (next) {
-  const event = Object.assign({}, this.request.body, {timestamp: new Date()})
-
   try {
+    const event = Object.assign({}, this.request.body, {timestamp: new Date()})
     yield r.db(config.rethinkdb.db).table('events').insert(event)
+
     this.response.status = 201
   } catch (error) {
     this.response.status = 500
