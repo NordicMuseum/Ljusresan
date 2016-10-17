90102d1ebdd1ba670b29bd2084440e2bdfdba159
Carl Calderon <carl@earthpeople.se>
2016-10-17 17:31:21 +0200

    Info and status section styling.

--- browser/client/containers/App/index.js
+++ browser/client/containers/App/index.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { TweenMax } from 'gsap'
 import 'reset-css/reset.css'
 
 import * as SessionActions from '../../actions/session'
@@ -12,6 +13,21 @@ class App extends Component {
 
   componentDidMount () {
     this.startPolling()
+
+    // set initial opacity
+    TweenMax.to('#status-section', 0, {
+      opacity: 0
+    })
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (
+      nextProps.session &&
+      this.props.session.updatedAt &&
+      nextProps.session.updatedAt !== this.props.session.updatedAt) {
+
+      this.transitionToStatusSection()
+    }
   }
 
   startPolling () {
@@ -22,6 +38,16 @@ class App extends Component {
     }, POLLING_FREQUENCY))
   }
 
+  transitionToStatusSection () {
+    TweenMax.to('#info-section', 0.5, {
+      opacity: 0
+    })
+    TweenMax.to('#status-section', 1, {
+      opacity: 1,
+      delay: 0.5
+    })
+  }
+
   render() {
     const { session, actions, children } = this.props
     return (
@@ -29,7 +55,7 @@ class App extends Component {
         <h1>Ljusresan • The Journey of Light</h1>
         <section id="info-section" className={style['info-section']}>
           <div className={style['flame']}>flame</div>
-          <div className={style['initial-copy']}>
+          <div className={style['copy-content']}>
             <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
   quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -41,6 +67,23 @@ class App extends Component {
           </div>
         </section>
         <section id="status-section" className={style['status-section']}>
+          <ol className={style['rooms']}>
+            <li className={style['room-1']}></li>
+            <li className={style['room-2']}></li>
+            <li className={style['room-3']}></li>
+            <li className={style['room-4']}></li>
+            <li className={style['room-5']}></li>
+          </ol>
+          <div className={style['copy-content']}>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat.</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat.</div>
+          </div>
         </section>
       </div>
     )
