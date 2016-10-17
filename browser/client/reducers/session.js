c8b33a497b30f7c5c9ab2d8572888d184a4359b1
Carl Calderon <carl@earthpeople.se>
2016-10-17 09:37:24 +0200

    Session reducer is now parsed properly.

--- browser/client/reducers/session.js
+++ browser/client/reducers/session.js
@@ -7,12 +7,33 @@ const initialState = {
   stations: []
 }
 
+const stationsPerRoom = {
+  1: 1,
+  2: 3,
+  3: 6,
+  4: 8,
+  5: 6,
+  6: 1
+}
+
 export default handleActions({
   'sync session' (state, action) {
     return {
       updatedAt: action.payload.updatedAt,
       tagUid: action.payload.tagUid,
-      stations: action.payload.stations
+      stations: action.payload.stations,
+      rooms: action.payload.stations.reduce((previous, data, index, origin) => {
+        const [room, station] = data.split(':')
+        console.log(previous)
+        if (!previous[room - 1]) {
+          previous[room - 1] = {complete: false, stations: []}
+          console.log('new room')
+        }
+        const target = previous[room - 1]
+        target.stations.push(station)
+        target.complete = stationsPerRoom[room] === target.stations.length
+        return previous
+      }, [])
     }
   }
 }, initialState)
