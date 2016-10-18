7f652edc046cacfcf81580c42c998520ae01eea1
Adam Agnaou <adam@earthpeople.se>
2016-10-18 14:17:24 +0200

    Add lights mapping table

--- api/src/config.js
+++ api/src/config.js
@@ -13,5 +13,10 @@ module.exports = {
       port: process.env.DMX_PORT,
       desc: 'Telnet test host. Only usable within the docker setup.'
     }
+  },
+
+  lights: {
+    '2:2': '_T02_P01_D01_ON_',
+    '2:3': '_T02_P02_D01_ON_'
   }
 }
