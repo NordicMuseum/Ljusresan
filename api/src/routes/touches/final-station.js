dd7d8f3651975b790d47d509b9b620f21ca36487
Adam Agnaou <adam@earthpeople.se>
2016-10-19 15:55:38 +0200

    Find last ended session by tagUid

--- api/src/routes/touches/final-station.js
+++ api/src/routes/touches/final-station.js
@@ -1,13 +1,19 @@
 const Session = require('../../models/session')
 
 module.exports = function * (next) {
-  let session = yield Session.exists('endedAt').sort('endedAt', -1).findOne()
+  const session = yield Session.where({
+    tagUid: this.session.tagUid,
+    ended: true
+  }).sort({
+    updated_at: -1
+  }).findOne()
 
   if (session) {
     this.response.body = {
       tagUid: session.get('tagUid'),
       stations: session.get('stations'),
-      endedAt: session.get('endedAt')
+      createdAt: session.get('created_at'),
+      updatedAt: session.get('updated_at')
     }
 
     this.response.status = 200
