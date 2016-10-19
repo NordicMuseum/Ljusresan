dc6396bb35ccba7abf6a59d9defe14c0da2aec55
Adam Agnaou <adam@earthpeople.se>
2016-10-19 14:22:02 +0200

    Refactor

--- api/src/routes/touches/create.js
+++ api/src/routes/touches/create.js
@@ -1,31 +1,37 @@
-const config = require('../../config')
-const Light = require('../../modules/light')
-const union = require('lodash').union
+const light = require('../../modules/light')
+const union = require('lodash/union')
 
 module.exports = function * (next) {
+  const session = this.session
   const {action, staticUserData: {room, station}} = this.request.body
 
+  const isFirstStation = (room === 1 && station === 1)
+  const isFinalStation = (room === 5 && station === 27)
+
   if (action === 'touch') {
-    const light = new Light()
-    const command = config.lights[`${room}:${station}`]
-
-    light.turnOn(command)
-    light.close()
-  }
-
-  if (action === 'remove') {
-    if (room === 1 && station === 1) {
-      this.session.set('stations', [])
-    } else if (station === 27) {
-      this.session.set('finalStationTimestamp', new Date())
-    } else {
-      this.session.set('stations', union(
-        this.session.get('stations'), [`${station}:${room}`]
-      ))
+    try {
+      light.on(room, station)
+      this.status = 204
+    } catch (error) {
+      this.status = 404
     }
   }
 
-  yield this.session.save()
+  if (action === 'remove') {
+    if (isFirstStation) {
+      session.set('stations', [])
+    } else if (isFinalStation) {
+      session.set('finalStationTimestamp', new Date())
+    } else {
+      session.set('stations', union(
+        session.get('stations'), [`${station}:${room}`]
+      ))
 
-  this.response.status = 200
+      light.off(room, station)
+    }
+
+    yield session.save()
+
+    this.response.status = 204
+  }
 }
