34f57ca153f1f73a91dfc41f3d86b823bfb1e294
Adam Agnaou <adam@earthpeople.se>
2016-10-20 22:16:08 +0200

    Always return stations key

--- api/src/routes/statuses/show.js
+++ api/src/routes/statuses/show.js
@@ -1,6 +1,8 @@
 module.exports = function * (next) {
   if (this.session) {
-    this.response.body = this.session.get('stations')
+    this.response.body = {
+      stations: this.session.get('stations')
+    }
 
     this.response.status = 200
   } else {
