ec68dd754ccc4b0cb379e086fdf342abc55fba24
Adam Agnaou <adam@earthpeople.se>
2016-10-20 16:36:39 +0200

    Respond with stations

--- api/src/routes/statuses/show.js
+++ api/src/routes/statuses/show.js
@@ -1,8 +1,6 @@
 module.exports = function * (next) {
   if (this.session) {
-    this.response.body = {
-      stations: this.session.get('stations')
-    }
+    this.response.body = this.session.get('stations')
 
     this.response.status = 200
   } else {
