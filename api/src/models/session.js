6515a0be50461f1e000666a775805bc3cbff2d0e
Adam Agnaou <adam@earthpeople.se>
2016-10-20 16:38:21 +0200

    Refactor validate and toggleLight functions

--- api/src/models/session.js
+++ api/src/models/session.js
@@ -1,15 +1,8 @@
 const config = require('../config')
 const Model = require('mongorito').Model
 const has = require('lodash/has')
-const difference = require('lodash/difference')
 const dmx = require('../modules/DMX')
-
-const parseDestination = (destination) => {
-  return {
-    room: destination.split(':')[0],
-    station: destination.split(':')[1]
-  }
-}
+const flatten = require('flat')
 
 module.exports = class Session extends Model {
   collection () {
@@ -28,24 +21,20 @@ module.exports = class Session extends Model {
   }
 
   * validate () {
-    const destination =
-      difference(this.attributes.stations, this.previous.stations)[0]
-
-    if (destination) {
-      const {room, station} = parseDestination(destination)
-
-      if (!has(config.commandMapping, [room, station])) {
+    if (this.changed.stations) {
+      const path = Object.keys(flatten(this.changed.stations))[0]
+      if (!has(config.commandMapping, path)) {
         throw new Error('Destination not found in commandMapping')
       }
     }
   }
 
   * toggleLight () {
-    const destination =
-      difference(this.attributes.stations, this.previous.stations)[0]
+    if (this.changed.stations) {
+      const obj = Object.keys(flatten(this.changed.stations))[0].split('.')
+      const room = obj[0]
+      const station = obj[1]
 
-    if (destination) {
-      const {room, station} = parseDestination(destination)
       dmx.on(room, station)
     }
   }
